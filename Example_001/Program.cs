8472be65c7119452f21ac2840188e7e83b62aa7a
Sergey Neskoromniy <sneskoromniy@gmail.com>
2023-08-02 00:19:42 +0200

    Задача 19 решена без методов

--- Example_001/Program.cs
+++ Example_001/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача принимает на вход координаты и выдает номер четверти
+Console.Clear();
+
+int X;
+while (true) // проверка на корректность ввода целого числа
+{
+Console.WriteLine("ВВЕДИТЕ X: ");
+if (int.TryParse(Console.ReadLine(), out X))
+    break;
+Console.WriteLine("Число не является целым");
+}
+int Y;
+while (true) // проверка на корректность ввода целого числа
+{
+Console.WriteLine("ВВЕДИТЕ Y: ");
+if (int.TryParse(Console.ReadLine(), out Y))
+    break;
+Console.WriteLine("Число не является целым");
+}
+
+Console.Write($"{X},{Y}");*/
+Console.Clear();
+int X, Y;
+static void InitCoordinatesByUser (ref init x, ref init y;
+{
+try
+{
+Console.WriteLine("ВВЕДИТЕ X: ");
+x = int.Parse(Console.ReadLine()?? "");
+
+Console.WriteLine("ВВЕДИТЕ X: ");
+y = int.Parse(Console.ReadLine()?? "");
+}
+
+catch(Exception exc)
+{
+    Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+    return;
+}
+}
+
+
+
+Console.Write($"{X},{Y}");
---
+++ Task_19/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.Clear();
+
+int num;
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Введите пятизначное число: ");
+        num = int.Parse(Console.ReadLine() ?? "");
+        break;
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+    }
+
+}
+
+if (num < 10000 || num > 99999)
+{
+    Console.WriteLine("Число не является пятизначным");
+}
+else
+{
+    int sim1 = num / 100;
+    int simTMP = num % 1000;
+    int sim2 = (simTMP % 10 * 100) + (simTMP / 10 % 10) * 10 + (simTMP / 100);
+    if (sim1 == sim2)
+        Console.WriteLine($"Число {num} палиндром");
+    else
+        Console.WriteLine($"Число {num} не палиндром");
+}
+
