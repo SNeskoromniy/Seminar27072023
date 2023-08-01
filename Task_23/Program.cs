6e2376a9bfbf195d894a4ca7b2d1186dac43e811
Sergey Neskoromniy <sneskoromniy@gmail.com>
2023-08-02 00:51:43 +0200

    Задача 23 решение без методов

---
+++ Task_23/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+Console.Clear();
+
+int num;
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Введите пятизначное число: ");
+        num = int.Parse(Console.ReadLine() ?? "");
+        break;
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+    }
+
+}
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write($" куб числа {i} равен: {i * i * i}");
+    Console.WriteLine();
+}
