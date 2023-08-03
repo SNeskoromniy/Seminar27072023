36e9cd3445fa08d23d6528c45de2084acdd53a3f
Sergey Neskoromniy <sneskoromniy@gmail.com>
2023-08-03 17:37:23 +0200

    Задача №28

---
+++ Task_28/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+PrintArr(GenRandomArr(8));
+
+
+// объявление методов
+// Метод заполнения массива случайными числами 0 и 1
+int[] GenRandomArr(int count)
+{
+    int[] arr = new int[count];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+
+}
