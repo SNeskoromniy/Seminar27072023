8838bf5eb6f2b0bb5a964bff482f8c43a1cbd0f6
Sergey Neskoromniy <sneskoromniy@gmail.com>
2023-08-06 08:25:19 +0200

    Task_32 Решение Задачи, №32 Task33 Решение задачи 33

---
+++ Task_32/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 32: Напишите программу замены элементов массива: положительные элементы замените на
+соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+*/
+
+Console.Clear();
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(string.Join(" ", array));
+
+int[] replace = GetReplace(array);
+Console.WriteLine(string.Join(" ", replace));
+
+// объявление методов
+// Метод заполнения массива случайными числами 0 и 1
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+//Метод расчета суммы положительных чисел 
+int[] GetReplace(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+    return arr;
+}
---
+++ Task_33/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Задача 33: Задайте массив. Напишите программу, которая
+определяет, присутствует ли заданное число в массиве.*/
+
+Console.Clear();
+
+int[] array = GetArray(12, 0, 9);
+Console.WriteLine(string.Join(" ", array));
+
+int num = GetNumberFromUser();
+
+bool result = FindNumber(array, num);
+
+Console.WriteLine($"число {num} = {(result ? "ДА" : "НЕТ")}");
+
+
+
+// объявление методов
+// Метод заполнения массива случайными числами 0 и 1
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+// Определяем функцию, выполняющую ввод целого числа с консоли
+static int GetNumberFromUser()
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите целое число: ");
+            int num = int.Parse(Console.ReadLine() ?? "");
+            return num;
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        }
+    }
+}
+
+//Функция поиска числа в массиве
+
+bool FindNumber(int[] arr, int num)
+{
+    bool result = false;
+    foreach (var el in array)
+    {
+        if (el == num) result = true;
+    }
+    return result;
+}
+
