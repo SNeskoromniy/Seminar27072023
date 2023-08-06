a852c36202483fdd948ff0aaac18ea6afb131e62
Sergey Neskoromniy <sneskoromniy@gmail.com>
2023-08-06 21:37:43 +0200

    Task_38 решение Задачи №38

--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -3,39 +3,64 @@
 элементом массива*/
 
 Console.Clear();
-
-double[] array = GetDoubleNumbersArray(4, 5, 10);
+//Формируем массив
+double[] array = GetDoubleNumbersArray(10, 0, 10, 3); //размер массива, диапазон значений массива, кол-во знаков после запятой
 Console.WriteLine(string.Join(" ", array));
-
-double result = GetResult(array);
+// Выводим минимальное число массива
+double min = GetMinNumber(array);
+Console.WriteLine($"минимальное число массива: {min}");
+// выводим максимальное число массива
+double max = GetMaxNumber(array);
+Console.WriteLine($"максимальное число массива: {max}");
+//Расчет разности между max и min
+double result = max - min;
 //Console.WriteLine($"минимальное число: {min}, максимальное число {max}, разница равна: {result} ");
 Console.WriteLine($"разница чисел равна: {result} ");
-
-
-
-//double[] min = GetMinNumber(array);
-//Console.WriteLine($"минимальное число массива: {min}");
-//double min = GetMaxNumber(array);
-//Console.WriteLine($"максимальное число массива: {max}");
-//double result = max - min;
-//Console.WriteLine($"минимальное число массива: {min}");
 
 //------- Конец программы----------
 
 
 // объявление методов
-// Метод заполнения массива случайными трехзначными числами 0 и 1
-double[] GetDoubleNumbersArray(int size, double minValue, double maxValue)
+// Метод заполнения массива случайными вещественными числами с округлением до 2 знаков после запятой
+double[] GetDoubleNumbersArray(int size, double minValue, double maxValue, int round)
 {
     double[] res = new double[size];
 
     for (int i = 0; i < size; i++)
     {
-        res[i] = (new Random().NextDouble() * (maxValue - minValue));
+        res[i] = Math.Round((new Random().NextDouble() * (maxValue - minValue)), round);
     }
     return res;
 }
 
+//Метод поиска минимального числа
+double GetMinNumber(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+        //if (array[i] > max) max = array[i];
+
+    }
+    return min;
+}
+
+//Метод поиска максимального числа
+double GetMaxNumber(double[] array)
+{
+    double max = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+    }
+    return max;
+}
+
+/* Метод получения разницы между максимальным и минимальным элементом массива
 double GetResult(double[] array)
 {
     double min = array[0];
@@ -50,3 +75,4 @@ double GetResult(double[] array)
     double result = max - min;
     return result;
 }
+*/
