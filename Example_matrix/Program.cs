34a3c36a6b8e09426d1e6d4d416887627951911b
Sergey Neskoromniy <sneskoromniy@gmail.com>
2023-08-22 19:28:16 +0200

    Пример работ с числовым массивом

---
+++ Example_matrix/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Заполнение матрицы случайными числами
+*/
+
+Console.Clear();
+
+
+
+//Инициализация массива 
+int[,] matrix = new int[3, 4];
+//ПЕчатае пустой массив
+PrintArray(matrix);
+//Заполняем массив случайными числами
+FillArray(matrix);
+Console.WriteLine();
+//Печать массива заполненного случайными числами
+PrintArray(matrix);
+
+
+//Метод печати массива
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+//Метод заполнения массива случайными числами от 1 до 9
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);//[1; 10)
+        }
+    }
+}
+
+
