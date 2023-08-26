635f2295a3f01a4e7d89e787fa74d350651b31a8
Sergey Neskoromniy <sneskoromniy@gmail.com>
2023-08-26 21:33:53 +0200

    задача 51

--- Task_51/Program.cs
+++ Task_51/Program.cs
@@ -1,21 +1,21 @@
-﻿/*Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+﻿/*Задача 51: Задайте двумерный массив. Найдите сумму элементов,
+находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
 Например, задан массив:
 1 4 7 2
 5 9 2 3
 8 4 2 4
-Среднее арифметическое
-каждого столбца: 4,6; 5,6; 3,6; 3.
+Сумма элементов главной диагонали: 1+9+2 = 12
 */
 
 #region --- 00. Configuration ---
 Console.Clear();
-Console.Title = "Задача №52";
+Console.Title = "Задача №51";
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 var curConsoleColor = Console.ForegroundColor;
 Console.ForegroundColor = ConsoleColor.DarkGreen;
 Console.WriteLine(@"************************************************************************
-Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
 
 ************************************************************************");
 Console.ForegroundColor = curConsoleColor;
@@ -23,11 +23,10 @@ Console.ForegroundColor = curConsoleColor;
 //Ввод размера массива
 int numI = GetNumberFromUser("введите количество строк массива: ");
 int numJ = GetNumberFromUser("введите количество столбцов массива: ");
+int sumDiag = 0;
 
 //Инициализация массива 
 int[,] matrix = new int[numI, numJ];
-double[] resultArray = new double[matrix.GetLength(1)];
-
 
 //Заполняем массив случайными числами
 FillArray(matrix);
@@ -35,10 +34,10 @@ FillArray(matrix);
 //Печать массива заполненного случайными числами
 PrintArray(matrix);
 //Вызов функции расчета среднего арифметического по столбцам
-resultArray = GetArithmeticMeanNum(matrix);
+sumDiag = GetSumDiag(matrix);
 
-Console.WriteLine("Результат расчета среднего арифметического по столбцам: ");
-Console.WriteLine(string.Join("; ", resultArray));
+Console.WriteLine($"Сумма элементов главной диагонали равна: {sumDiag}");
+
 //Конец программы
 
 //Определение методов
@@ -84,19 +83,16 @@ void FillArray(int[,] matr)
         }
     }
 }
-//Метод расчета среднего арифметического по столбцам массива
-double[] GetArithmeticMeanNum(int[,] matrix)
+//Метод расчета суммы главной диагонали
+int GetSumDiag(int[,] matrix)
 {
-    double[] resultArr = new double[matrix.GetLength(1)];
-    double sum = 0;
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            sum = sum + matrix[i, j];
+            if (i == j) sum = sum + matrix[i, j];
         }
-        resultArr[j] = sum / matrix.GetLength(1);
-        sum = 0;
     }
-    return resultArr;
+    return sum;
 }
